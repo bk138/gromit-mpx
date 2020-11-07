d7133eeecc9de21247144fa5b46108a853633803
Christian Beier <info@christianbeier.net>
2020-11-07 13:02:06 +0100

    Add menu item to display man page

--- src/callbacks.h
+++ src/callbacks.h
@@ -126,6 +126,9 @@ void on_about(GtkMenuItem *menuitem,
 void on_intro(GtkMenuItem *menuitem,
 	      gpointer user_data);
 
+void on_man_page(GtkMenuItem *menuitem,
+		 gpointer user_data);
+
 void on_support_liberapay(GtkMenuItem *menuitem,
 			  gpointer user_data);
 
